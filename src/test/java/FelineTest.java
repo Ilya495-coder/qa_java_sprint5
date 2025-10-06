13df82ea108b536c18d42bb8ae5f4559e83ca838
Илюха <goncharovson@mail.ru>
2025-10-06 11:34:49 +0300

    Добавлен тестовый экземпляр класса. Настроено мок-поведение метода: возвращает значение 1.

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -7,29 +7,36 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 public class FelineTest {
+
     @Spy
     Feline feline;
+
+    Feline feline1 = new Feline();
+
     @Test
     public void testeatMeatCheckMethodCall() throws Exception{
         feline.eatMeat();
         Mockito.verify(feline,Mockito.times(1)).getFood("Хищник");
     }
+
     @Test
     public void testGetFamily(){
-        String actual = feline.getFamily();
+        String actual = feline1.getFamily();
         assertEquals("Кошачьи", actual);
         System.out.println("Актуальное значение: " + actual + ", ожидаемое значение: Кошачьи");
     }
+
     @Test
     public void testGetKittens(){
-        Mockito.when(feline.getKittens(Mockito.anyInt())).thenReturn(2);
+        Mockito.when(feline.getKittens(Mockito.anyInt())).thenReturn(1);
         int actual = feline.getKittens();
-        assertEquals(2,actual);
-        System.out.println("Актуальное значение: " + actual+ ", ожидаемое значение: 2");
+        assertEquals(1,actual);
+        System.out.println("Актуальное значение: " + actual+ ", ожидаемое значение: 1");
     }
+
     @Test
     public void testGetKittensWithParameters(){
-        int actual = feline.getKittens(50);
+        int actual = feline1.getKittens(50);
         assertEquals(50,actual);
         System.out.println("Актуальное значение: " + actual+ ", ожидаемое значение: 50");
     }
